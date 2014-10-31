bfd50e9418571ab7c8e472afa230813adbfedc2a
Raymond Scott <genji@Genji-12.home>
2014-10-30 22:56:43 -0500

    second container working

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -8,20 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
     
     var firstContainer: UIView!
     var secondContainer: UIView!
     var thirdContainer: UIView!
     var fourthContainer: UIView!
     
+    var titleLabel: UILabel!
+    
     let kMarginForView:CGFloat = 10.0
+    let kMarginForSlot:CGFloat = 2.0
     let kSixth:CGFloat = 1.0/6.0
+    let kThird:CGFloat = 1.0/3.0
+    
+    let kNumberOfContainers = 3
+    let kNumberOfSlots = 3
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setupContainerViews()
+        setupFirstContainer(self.firstContainer)
+        setupSecondContainer(self.secondContainer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,7 +58,30 @@ class ViewController: UIViewController {
         self.fourthContainer.backgroundColor = UIColor.blackColor()
         self.view.addSubview(self.fourthContainer)
     }
-
-
+    
+    func setupFirstContainer(containerView: UIView)
+    {
+        self.titleLabel = UILabel()
+        self.titleLabel.text = "Super Slots"
+        self.titleLabel.textColor = UIColor.yellowColor()
+        self.titleLabel.font = UIFont(name: "MarkerFelt-Wide", size: 40)
+        self.titleLabel.sizeToFit()
+        self.titleLabel.center = containerView.center
+        containerView.addSubview(self.titleLabel)
+    }
+    
+    func setupSecondContainer (containerView: UIView)
+    {
+        for var containerNumber = 0; containerNumber < kNumberOfContainers; ++containerNumber
+        {
+            for var slotNumber = 0; slotNumber < kNumberOfSlots; ++slotNumber
+            {
+                var slotImageView = UIImageView()
+                slotImageView.backgroundColor = UIColor.yellowColor()
+                slotImageView.frame = CGRect(x: containerView.bounds.origin.x + (containerView.bounds.size.width * CGFloat(containerNumber) * kThird), y: containerView.bounds.origin.y + (containerView.bounds.size.height * CGFloat (slotNumber) * kThird), width: containerView.bounds.width * kThird - kMarginForSlot, height: containerView.bounds.height * kThird - kMarginForSlot)
+                containerView.addSubview(slotImageView)
+            }
+        }
+    }
 }
 
