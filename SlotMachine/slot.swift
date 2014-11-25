a32361b2bab5ab08bb714fa2eb50a5b184f53d2d
Raymond Scott <genji@Genji-3.local>
2014-11-25 16:14:22 -0600

    assets added.

---
+++ SlotMachine/slot.swift
@@ -0,0 +1,18 @@
+//
+//  slot.swift
+//  SlotMachine
+//
+//  Created by Raymond Scott on 11/25/14.
+//  Copyright (c) 2014 Raymond Scott. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot
+{
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+    
+}
